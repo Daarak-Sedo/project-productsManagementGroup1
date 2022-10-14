74f381b80e43bdf2c79b80fbe9d8aa0db048b4d9
Bhuwan Singh Samant <107810689+Daarak-Sedo@users.noreply.github.com>
2022-10-14 08:20:15 +0530

    Update validator.js

---
+++ src/validator/validator.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+
+
+const isValidObjectId = function (objectId) {
+    
+        return mongoose.Types.ObjectId.isValid(objectId)
+    }
+    
+    
+
+module.exports.isValidObjectId = isValidObjectId;
