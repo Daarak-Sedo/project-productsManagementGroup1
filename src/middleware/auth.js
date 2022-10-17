87e1b8a1906c3ec3af3485931bea01ebf78201ff
Shayan Biswas <106047915+shbis@users.noreply.github.com>
2022-10-17 16:10:53 +0530

    Update auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,44 +1,35 @@
 import jwt from 'jsonwebtoken';
 
-// const auth = async (req, res, next) => {
-//     try {
-//         const token = req.headers.authorization.slice(7,);
-//         console.log(token)
-
-//         if (!token)
-//             res.status(400).send({ status: false, message: `Token must be present.` })
-//         jwt.verify(token, 'group1', (err, decoded) => {
-//             if (err)
-//                 res.status(400).send({ status: false, message: `Authentication Failed!`, error: err.message })
-//             req['user'] = decoded.userId
-//             next()
-//         })
-//     }
-//     catch (err) {
-//         res.status(500).send({ status: false, error: err.message })
-//     }
-// }
-
-
-const auth = async (req, res, next) => {
+const authentication = async (req, res, next) => {
     try {
         let token = req.headers.authorization
-        if (!token) return res.status(400).send({ status: false, message: `token must be present.` });
+        if (!token)
+            res.status(400).send({ status: false, message: `Token must be present.` })
 
-        token = token.split(' ')[1];
+        token = req.headers.authorization.slice(7);
 
-        jwt.verify(token, 'group1', (err, decoded) =>{
-            if (err) 
-                return res.status(401).send({ status: false, message: err.message })
-
-                req['user'] = decoded.userId
-                next()
+        jwt.verify(token, 'group1', (err, decoded) => {
+            if (err)
+                res.status(400).send({ status: false, message: `Authentication Failed!`, error: err.message })
+            req['user'] = decoded.userId
+            next()
         })
     }
     catch (err) {
-        return res.status(500).send({ status: false, message: err.message })
+        res.status(500).send({ status: false, error: err.message })
     }
 }
 
+const authorization = async (req, res, next) => {
+    try {
+        const userId = req.params.userId
+        if (req.user != userId)
+        next()
+    }
+    catch (err) {
+        res.status(500).send({ status: false, error: err.message })
+    }
+}
 
-export default auth;
+export { authentication, authorization };
+
