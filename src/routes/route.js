7751fe62e9df60cf66a9e51c3cb53d6495c5ff60
Shayan Biswas <106047915+shbis@users.noreply.github.com>
2022-10-17 16:26:44 +0530

    Update route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const router = express.Router();
 import { createUser, login, gateUser, updateUser } from '../controllers/userController.js';
-import { createProduct, getProducts, getProductById, updateProduct, deleteProduct } from '../controllers/productController.js';
+import { createProduct, getProductByQuery, getProductById, updateProduct, deleteProduct } from '../controllers/productController.js';
 import { createCart, updateCart, getCart, deleteCart } from '../controllers/cartController.js';
 import { createOrder, updateOrder } from '../controllers/orderController.js';
 import { authentication, authorization } from '../middleware/auth.js';
@@ -10,27 +10,25 @@ import { authentication, authorization } from '../middleware/auth.js';
 //--------------FEATURE I - User---------------->
 router.post('/register', createUser);  //aj
 router.post('/login', login);          //sa
-router.get('/user/:userId/profile', auth, gateUser);    //shayan
-router.get('/user/:userId/profile', auth, gateUser);    //shayan
-router.put('/user/:userId/profile', auth, updateUser);  //shayan
+router.get('/user/:userId/profile', authorization, gateUser);   //shayan
+router.put('/user/:userId/profile', authentication, authorization, updateUser); //shayan
 
 //-------------FEATURE II - Product--------------->
-router.post('/products', createProduct);   //aj
-router.get('/products', getProducts);      //shayan
-router.get('/products/:productId', getProductById);   //shayan
+router.post('/products', createProduct);               //aj
+router.get('/products', getProductByQuery);            //shayan
+router.get('/products/:productId', getProductById);     //shayan
 router.put('/products/:productId', updateProduct);     //sa
 router.delete('/products/:productId', deleteProduct)   //shayan
 
-
 //------------FEATURE III - cart--------------->
-router.post('/users/:userId/cart', createCart);     //aj
-router.put('/users/:userId/cart', updateCart);      //bbbbb
-router.get('/users/:userId/cart', getCart);         //shayan  
-router.delete('/users/:userId/cart', deleteCart);   //shayan  
+router.post('/users/:userId/cart', authentication, authorization, createCart);        //aj
+router.put('/users/:userId/cart', authentication, authorization, updateCart);         //bbbbb
+router.get('/users/:userId/cart', authentication, authorization, getCart);            //shayan  
+router.delete('/users/:userId/cart', authentication, authorization, deleteCart);      //shayan  
 
 //-------------FEATURE IV - Order--------------->
-router.post('/users/:userId/orders', createOrder); //bbbb
-router.put('/users/:userId/orders', updateOrder);  //sana
+router.post('/users/:userId/orders', authentication, authorization, createOrder);   //bbbb
+router.put('/users/:userId/orders', authentication, authorization, updateOrder);    //sana
 
 
 export default router;
