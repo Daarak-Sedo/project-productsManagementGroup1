62d4438a212f344b3d4280d1e1923862d918b303
Shayan Biswas <106047915+shbis@users.noreply.github.com>
2022-10-17 16:10:20 +0530

    Update route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,27 +4,27 @@ import { createUser, login, gateUser, updateUser } from '../controllers/userCont
 import { createProduct, getProducts, getProductById, updateProduct, deleteProduct } from '../controllers/productController.js';
 import { createCart, updateCart, getCart, deleteCart } from '../controllers/cartController.js';
 import { createOrder, updateOrder } from '../controllers/orderController.js';
-import auth from '../middleware/auth.js';
+import { authentication, authorization } from '../middleware/auth.js';
 
 
 //--------------FEATURE I - User---------------->
 router.post('/register', createUser);  //aj
 router.post('/login', login);          //sa
-router.get('/user/:userId/profile', auth, gateUser);   //shayan
-router.get('/user/:userId/profile', auth, gateUser);  //shayan
+router.get('/user/:userId/profile', auth, gateUser);    //shayan
+router.get('/user/:userId/profile', auth, gateUser);    //shayan
 router.put('/user/:userId/profile', auth, updateUser);  //shayan
 
 //-------------FEATURE II - Product--------------->
 router.post('/products', createProduct);   //aj
 router.get('/products', getProducts);      //shayan
-router.get('/products', getProductById);   //shayan
+router.get('/products/:productId', getProductById);   //shayan
 router.put('/products/:productId', updateProduct);     //sa
 router.delete('/products/:productId', deleteProduct)   //shayan
 
 
 //------------FEATURE III - cart--------------->
-router.post('/users/:userId/cart', createCart); //aj
-router.put('/users/:userId/cart', updateCart);  //bbbbb
+router.post('/users/:userId/cart', createCart);     //aj
+router.put('/users/:userId/cart', updateCart);      //bbbbb
 router.get('/users/:userId/cart', getCart);         //shayan  
 router.delete('/users/:userId/cart', deleteCart);   //shayan  
 
